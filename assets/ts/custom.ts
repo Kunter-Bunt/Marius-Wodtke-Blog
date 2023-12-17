6b9d6d3b09e5ede219fd894d4816f6eb98488f76
Marius Wodtke <marius.wodtke@gmail.com>
2023-12-17 17:06:27 +0100

    Attempted different viewer count for dsgvo conformant tracking

--- assets/ts/custom.ts
+++ assets/ts/custom.ts
@@ -7,10 +7,47 @@ class MwoTrack {
                 body: JSON.stringify({page: page})
             });
     }
+
+    public static TrackAnonymous2() {
+        const data = {
+            id: this.uuidv4(),
+            page: window.location.pathname,
+            date: new Date().toISOString()
+          };
+        
+          const gql = `
+            mutation create($item: CreatePageView!) {
+              createPageView(item: $item) {
+                id
+                page
+                date
+              }
+            }`;
+          
+          const query = {
+            query: gql,
+            variables: {
+              item: data
+            } 
+          };
+
+          const endpoint = "/data-api/graphql";
+          const result = fetch(endpoint, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(query)
+          });
+    }
+
+    public static uuidv4() {
+        return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+          (<any>c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> <any>c / 4).toString(16)
+        );
+      }
 }
 
 window.addEventListener('load', () => {
     setTimeout(function () {
-        MwoTrack.TrackAnonymous();
+        MwoTrack.TrackAnonymous2();
     }, 0);
 })
